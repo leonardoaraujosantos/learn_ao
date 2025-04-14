ed09b35fd2af462584ef20b05fd2f9ae9a79e54a
Leonardo <leo@amini.ai>
2025-04-14 16:20:16 -0300

    Improve wallet button

--- frontend/src/lib/ButtonConnectWallet.svelte
+++ frontend/src/lib/ButtonConnectWallet.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import {connectWallet, disconnectWallet} from '../lib/wallet'
-    let isOpen = false;
+    import { isConnectedWallet } from '../stores/blockchainStore';
+    import { get } from 'svelte/store';
+    let isOpen = get(isConnectedWallet);
 
     function togglePanel() {
         isOpen = !isOpen;
     }
   
     async function handleConnect() {
+      const connected = get(isConnectedWallet);
       
-      if (!isOpen){
+      if (!connected){
         await connectWallet();
       } else {
         await disconnectWallet();
